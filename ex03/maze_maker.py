b9e0770d375e0e02fe794e7ea7d1b3bfaa885c04
sakamoto <c0a2105787@deu.teu.ac.jp>
2022-12-06 14:25:34 +0900

    練習3-3

--- ex03/maze_maker.py
+++ ex03/maze_maker.py
@@ -39,10 +39,17 @@ def print_maze(maze_lst):
 
 #maze_makerテスト用
 if __name__ == "__main__":
+    
     root= tk.Tk()
+
     root.title("迷えるこうかとん")
-    canvas = tk.Canvas(root,width=1500,height=900,bg="black")
+    canvas = tk.Canvas(root,width=1500,height=900,bg="black")#キャンバス設定
     canvas.pack()
+
+    kokaton = tk.PhotoImage(file="fig/0.png")#こうかとんの写真
+    cx,cy = 300,400
+    canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
+
     maze = make_maze(15,9)
     print_maze(maze)
 
