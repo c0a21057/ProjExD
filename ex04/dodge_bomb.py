b9c61565f43b0d433eeee91c3d87a4ab947f7d22
sakamoto <c0a2105787@deu.teu.ac.jp>
2022-12-13 14:31:39 +0900

    練習4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,16 +1,44 @@
 import pygame as pg
 import sys
 
+
 def main():
-    pg.display.set_caption("逃げろこうかとん")
-    scrn_sfc = pg.display.set_mode((1600,900))
+    clock =pg.time.Clock()
+    # 練習１
+    pg.display.set_caption("逃げろ！こうかとん")
+    scrn_sfc = pg.display.set_mode((1600, 900))
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
-    pgbg_sfc = pg.transform.rotozoom(pgbg_sfc, 0, 1.0)
     pgbg_rct = pgbg_sfc.get_rect()
-    pgbg_rct.center = 400, 300
-    scrn_sfc.blit(pgbg_sfc, pgbg_rct)
-    
-    pg.display.update()
+
+    # 練習３
+    tori_sfc = pg.image.load("fig/6.png")
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
+    tori_rct = tori_sfc.get_rect()
+    tori_rct.center = 900, 400
+    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける
+    scrn_sfc.blit(tori_sfc, tori_rct) 
+
+    # 練習２
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        key_dct = pg.key.get_pressed()
+        #練習4
+        if key_dct[pg.K_UP]:
+            tori_rct.centery -= 1
+        if key_dct[pg.K_DOWN]:
+            tori_rct.centerx += 1
+        if key_dct[pg.K_LEFT]:
+            tori_rct.centerx -= 1
+        if key_dct[pg.K_RIGHT]:
+            tori_rct.centery += 1
+        scrn_sfc.blit(tori_sfc, tori_rct)
+        
+
+        pg.display.update()
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
